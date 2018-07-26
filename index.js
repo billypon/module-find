ce9931578ce7f16a8519da9838aa10caccbc7d6b
billypon <billypon@gmail.com>
2018-07-27 00:57:57 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+var fs = require('fs');
+var path = require('path');
+
+var root = path.dirname(require.main.filename);
+while (root !== '/' && !fs.existsSync(root + '/node_modules')) {
+  root = path.dirname(root);
+}
+
+if (root === '/') {
+  return console.error('can\'t find project root');
+}
+
+root += '/node_modules/';
+
+function find() {
+  var modules = [];
+  [].forEach.call(arguments, function (array) {
+    array = array instanceof Array ? array : [array];
+    var module = path.dirname(root);
+    for (var i in array) {
+      var x = array[i];
+      module += '/node_modules/';
+      module = fs.existsSync(module + x) ? module + x :
+        module !== root ? path.resolve(module, '../..') : root;
+      if (module === root) {
+        module = undefined;
+        break;
+      }
+    }
+    modules.push(module);
+  });
+  return arguments.length > 1 ? modules : modules[0];
+}
+
+module.exports = function (value) {
+  if (value) {
+    if (!value.endsWith('/')) {
+      value += '/';
+    }
+    root = value + 'node_modules/';
+  }
+  return find;
+}
---
+++ test/test.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+var path = require('path');
+
+var cwd = process.cwd();
+var find = require('..')(cwd);
+cwd += '/node_modules/';
+
+it('undefined', function () {
+  assert.equal(undefined, find('fake-module'));
+});
+
+it('parent', function () {
+  assert.equal(cwd + 'dependency-parent', find('dependency-parent'));
+});
+
+it('parent && child', function () {
+  var dependencies = ['dependency-parent', 'dependency-child'];
+  find.apply(this, dependencies).forEach(function (x, i) {
+    assert.equal(cwd + dependencies[i], x);
+  });
+});
+
+it('child of parent', function () {
+  assert.equal(cwd + 'dependency-parent/node_modules/dependency-child', find(['dependency-parent', 'dependency-child']));
+});
