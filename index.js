9d82912124e8c6c98cbbe5d7efc675f1c3d1a537
billypon <billypon@gmail.com>
2018-07-28 16:06:31 +0800

    fix error thrown

--- index.js
+++ index.js
@@ -5,19 +5,16 @@ module.exports = function (root) {
   if (root === true) {
     root = path.dirname(require.main.filename);
   } else if (root) {
-    if (root.endsWith('/')) {
-      root = root.substr(0, root.length - 1);
-    }
+    root = root.endsWith('/') ? root.substr(0, root.length - 1) : root;
   } else {
     root = process.cwd();
   }
 
-  var origin = root;
   while (!fs.existsSync(root + '/node_modules')) {
-    root = path.dirname(root);
     if (root === '/') {
-      throw {message: 'can\'t find project root', path: origin};
+      throw new Error('can\'t find project root')
     }
+    root = path.dirname(root);
   }
 
   root += '/node_modules';
